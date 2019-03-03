f34c8cf9f863e703cf46af586652d03cb41f43b6
tony <tonytenorioc@gmail.com>
2019-03-03 10:06:48 -0600

    Adding animations and scroll spy effects to the project

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
 
+    document.querySelectorAll('.navbar .nav-link').forEach(enlace => {
+        enlace.addEventListener('click', function (e) {
+            e.preventDefault();
+    
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+
+    window.onscroll = function (e) {
+        let scroll = window.scrollY;
+    
+        const headerScroll = document.querySelector('#navegacion-principal');
+        if(scroll > 300) {
+            headerScroll.classList.add('bg-success');
+        } else {
+            headerScroll.classList.remove('bg-success');
+        }
+    };
+
     $('#fecha').countdown('2019/08/23', function(event) {
         $(this).html(event.strftime('<p><span> %D </span> dias <span> %H </span> horas <span> %M </span> minutos  <span> %S </span> Segundos </p>'));
     });
